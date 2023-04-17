61b60a0ddbc6e146b6ef0323e7c6378dee78efdd
razuvaevcr <51992031+razuvaevcr@users.noreply.github.com>
2023-04-17 17:39:49 +0300

    Revert "Sprint 1"

--- src/layouts/ErrorLayout.js
+++
@@ -1,5 +0,0 @@
-const errorLayout = `<div class="error__title">{{errorNumber}}</div>
-					<div class="error__subtitle">{{subtitle}}</div>
-					<a href="/#/home" class="error__link btn_flat">{{button}}</a>`;
-
-export default errorLayout;
